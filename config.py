01d382606910e99311e52f230040d245f79fb08b
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2022-12-26 17:18:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 
-user_token = 'vk1.a.ILtMqNvyS_6rVLxHecAiq2pfW43UNURB-UCiS-1zVpTcFVr5ibnI2-FgaWYYiyaTCGWwZkjpdqLcf55ZeWtrQwwUYebhwgvbtetmbal5lYVMKEFIMCj3BXJMTUpqy5frv1Uwvc5ZM99eePL6U7r-lAmHjP_x-Hj4boko91qbXkHf_E3vWVqlNoNX1nzYBUyP4wW5EgSv6AEmH7CFArB5Yg' # токен пользователя
-comm_token = 'vk1.a.MiZ4sOIcFSRImNckpzYWUnL3WQ4aNRWVMnIr7QmcmJDgby1VKuBV2NDIqpGw1ZV1yrBttzg77iwZO_0Tyu3V8HzVi-aJ5QlLhvE4WVnEcuODrl5YvFo0IO7uL9rZyppvp6i8AkC_Lpeg2LIXohdV9vGo_BnL2s71raR_wQQXzGG30lQm9VBP8KRrixsZztD7UtBHN8fR4Unt9CHlieQdEw' # токен сообщества
+user_token = '' # токен пользователя
+comm_token = '' # токен сообщества
 
 offset = 0 #сдвиг
 line = range(0, 1000) # последовательность для перебора найденных пользователей
 
-#host = '127.0.0.1'
-# user = 'postgres'
-# password = '0000'
-# db_name = 'bot_users'
