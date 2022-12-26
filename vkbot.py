64ecbc61c4cad8dcbbf5a73ef36abf6f9eb4816c
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2022-12-26 17:24:53 +0300

    Add files via upload

--- vkbot.py
+++ vkbot.py
@@ -4,7 +4,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from config import user_token, comm_token, offset, line
 import requests
 import datetime
-from 1 import *
+from fun import *
 
 
 session = vk_api.VkApi(token=comm_token)
@@ -23,7 +23,10 @@ for event in VkLongPoll(session).listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         text = event.text.lower()
         user_id = event.user_id
-        if text == "hello":
-            send_message(user_id, "something text")
-            find_user(user_id)
-            write_msg(event.user_id, f'Нашёл для тебя пару, жми на кнопку "Вперёд"')
+        if text == "привет":
+            vkbot.send_message(user_id, "привет")
+            vkbot.find_user(user_id)
+            creating_database()
+            vkbot.write_msg(event.user_id, f'Нашёл для тебя пару')
+            vkbot.find_persons(user_id, offset)
+
