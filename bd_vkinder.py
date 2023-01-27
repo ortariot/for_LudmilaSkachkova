1912bdf705beeb9334883e6da1197648dc4b362a
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2023-01-27 21:14:10 +0300

    Add files via upload

--- bd_vkinder.py
+++ bd_vkinder.py
@@ -9,77 +9,37 @@ connection = psycopg2.connect(
 )
 connection.autocommit = True
 
-def create_table_applicant():
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS applicant (
-                id_applicant SERIAL PRIMARY KEY,
-                vk_id_applicant varchar(20) UNIQUE NOT null);
-                """
-        )
-    print("[INFO] Table  was created.")
-
 def create_table_seen_users():
 
     with connection.cursor() as cursor:
         cursor.execute(
-            """CREATE TABLE IF NOT EXISTS seen_users(
-            id_seen_users serial PRIMARY KEY,
-            applicant_id INTEGER references applicant (id_applicant),
-            vk_id_seen_users varchar(20) UNIQUE NOT null);"""
+            "CREATE TABLE IF NOT EXISTS seen_users(\
+            id SERIAL PRIMARY KEY, \
+						vk_user_id varchar(20), \
+						vk_profile_id varchar(20);"
         )
-    return cursor.fetchone()
-
-
-def create_table_choice():
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS choice(
-            id_choice serial PRIMARY KEY,
-            applicant_id INTEGER references applicant (id_applicant),
-            vk_id_choice varchar(20) UNIQUE NOT null);"""
-        )
-    return cursor.fetchone()
-
-
-def insert_data_users(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """INSERT INTO applicant (vk_id_applicant)  
-            VALUES ( '{user_id}');""")
         return cursor.fetchone()
-
-def insert_data_seen_users(result_id, user_id):
+    
+def insert_seen_users(result_id, user_id):
     with connection.cursor() as cursor:
         cursor.execute(
-            """INSERT INTO seen_users (vk_id_seen_users, applicant_id,) 
+            """INSERT INTO seen_users (vk_user_id, vk_profile_id) 
             VALUES ('{result_id}', '{user_id}');""" )
         return cursor.fetchone()
-
-def insert_data_choice(result_id, user_id):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """INSERT INTO choice (vk_id_seen_users, applicant_id,) 
-            VALUES ('{result_id}', '{user_id}');""")
-        return cursor.fetchone()
-
-
+    
 def select(offset):
     with connection.cursor() as cursor:
         cursor.execute(
-            """select id_applicant from applicant a
-                        JOIN seen_users su
-                        ON a.id_applicant = su.applicant_id
-                        WHERE su.applicant_id IS NULL;"""
+            """select vk_profile_id from seen_users
+                WHERE vk_profile_id IS NULL;"""
         )
         return cursor.fetchone()
 
 def check(user_id):
     with connection.cursor() as cursor:
         cursor.execute(
-            """select EXISTS id_applicant from applicant a
-                       WHERE id_applicant = ?;""")
+            """select EXISTS vk_user_id from seen_users
+                       WHERE vk_user_id = ?;""")
         return cursor.fetchone()
         if cursor.fetchone() == "True":
             insert_data_users(user_id)
